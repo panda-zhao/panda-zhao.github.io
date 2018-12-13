3eab895ea8be545fa8bd476bdd6e12326141248e
zhaowy <zhaowy@3win.com>
2018-12-13 11:38:52 +0800

    2018/12/12(星期四)---改变结构

---
+++ js/common.js
@@ -0,0 +1,97 @@
+
+
+var data = [
+  // 前端框架
+  [
+    {name: "BootCDN",url: "http://www.bootcdn.cn",title: "免费的CDN加速服务"},
+    {name: "jquery",url: "http://jquery.cuishifeng.cn/",title: "jQuery API 3.3.1 速查表"},
+    {name: "Bootstrap3",url: "https://v3.bootcss.com/css/",title: ""},
+    {name: "layui",url: "http://www.layui.com",title: "经典模块化前端UI框架"},
+    {name: "Amaze UI",url: "http://amazeui.org/getting-started?_ver=2.x",title: "框架"},
+    {name: "font awesome 4.7.0",url: "https://fontawesome.com/v4.7.0/icons/",title: ""},
+    {name: "datatables",url: "http://www.datatables.club/",title: "表格插件"},
+    {name: "Echarts",url: "http://echarts.baidu.com/download.html",title: "图表插件"},
+    {name: "swiper4",url: "https://www.swiper.com.cn/",title: "轮播图插件"},
+    {name: "jQuery UI",url: "http://jqueryui.com/themeroller/",title: ""},
+    {name: "百度分享插件",url: "http://share.baidu.com/code",title: ""},
+  ],
+  //前端学习
+  [
+    {name: "javascript入门",url: "https://wangdoc.com/javascript/basic/introduction.html",title: ""},
+    {name: "JavaScript 标准参考教程（alpha）",url: "http://javascript.ruanyifeng.com/",title: ""},
+    {name: "ECMAScript 6",url: "http://es6.ruanyifeng.com/#docs/intro",title: ""},
+    {name: "MDN web docs",url: "https://developer.mozilla.org/zh-CN/",title: ""},
+    {name: "W3Cschool",url: "https://www.w3cschool.cn/",title: ""},
+    {name: "菜鸟教程",url: "http://www.runoob.com/",title: ""},
+    {name: "W3school",url: "http://www.w3school.com.cn/index.html",title: ""},
+    {name: "CSS3参考手册",url: "http://www.css88.com/book/css/quicksearch.htm",title: ""},
+    {name: "微信小程序",url: "https://mp.weixin.qq.com/cgi-bin/wx",title: ""},
+    {name: "百度小程序",url: "https://smartprogram.baidu.com/mappconsole/main/login",title: ""},
+    {name: "Normalize跨浏览器一致性",url: "http://necolas.github.io/normalize.css/",title: ""},
+    {name: "高德开发平台",url: "http://lbs.amap.com/api/javascript-api/guide/abc/quickstart",title: ""},
+    {name: "Web 前端技术图谱",url: "http://www.runoob.com/w3cnote/webfrontendstack.html",title: ""},
+    {name: "前端开发面试题",url: "http://www.runoob.com/w3cnote/front-end-developer-questions-and-answers.html",title: ""},
+  ],
+  // 前端工具
+  [
+    {name: "GitHub",url: "https://github.com/panda-zhao",title: ""},
+    {name: "图片压缩",url: "https://tinypng.com/",title: ""},
+    {name: "雪碧图合成",url: "https://www.toptal.com/developers/css/sprite-generator",title: ""},
+    {name: "坚果云",url: "https://www.jianguoyun.com/",title: ""},
+    {name: "七牛云",url: "https://www.qiniu.com/",title: ""},
+    {name: "全聚合",url: "https://www.juhe.cn/",title: "数据接口"},
+    {name: "在线文本比较工具",url: "http://www.jq22.com/textDifference",title: ""},
+    {name: "百度思维导图",url: "http://naotu.baidu.com/home",title: ""},
+    {name: "jsfiddle",url: "https://jsfiddle.net/",title: "在线编辑器"},
+    {name: "HTML代码转义和解码",url: "https://c.runoob.com/front-end/691",title: ""},
+    {name: "CSS3代码生产器",url: "http://www.css3maker.com/index.html#",title: ""},
+    {name: "CAN I USE?",url: "https://caniuse.com/",title: ""},
+    {name: "jshint语法检查",url: "http://jshint.com/",title: ""},
+  ],
+  // 个人建站
+  [
+    {name: "阿里云",url: "https://home.console.aliyun.com/new#/",title: ""},
+    {name: "百度统计",url: "https://tongji.baidu.com/web/25037646/overview/index",title: ""},
+    {name: "代码高亮",url: "http://www.592lvyou.com/?page_id=13036",title: ""},
+    {name: "制作网站logo",url: "http://www.logomaker.com.cn/",title: ""},
+    {name: "制作网站ico图标",url: "http://www.bitbug.net/",title: ""},
+    {name: "网站素材易企秀",url: "http://www.eqxiu.com/",title: "数据接口"},
+  ],
+  // vue.js项目
+  [
+    {name: "webpack中文教程",url: "https://doc.webpack-china.org/",title: ""},
+    {name: "Vue.js教程",url: "https://cn.vuejs.org/v2/guide/installation.html",title: ""},
+    {name: "Vue.js菜鸟教程",url: "http://www.runoob.com/vue2/vue-tutorial.html",title: ""},
+    {name: "Vuex教程",url: "https://vuex.vuejs.org/zh/",title: ""},
+    {name: "Vue Router",url: "https://router.vuejs.org/zh/",title: ""},
+    {name: "Axios中文说明",url: "https://www.kancloud.cn/yunye/axios/234845",title: ""},
+    {name: "Store.js",url: "https://www.bootcdn.cn/store.js/readme/",title: ""},
+    {name: "ElementUI教程",url: "http://element-cn.eleme.io/#/zh-CN/component/installation",title: ""},
+  ],
+  // 服务器端
+  [
+    {name: "Express",url: "http://www.expressjs.com.cn/",title: ""},
+    {name: "Node.js 中文网",url: "http://nodejs.cn/api/",title: ""},
+    {name: "node.js教程",url: "http://www.runoob.com/nodejs/nodejs-tutorial.html",title: ""},
+    {name: "淘宝npm镜像",url: "http://npm.taobao.org/",title: ""},
+  ],
+  // 案例网站
+  [
+    {name: "网易阴阳师官网",url: "https://yys.163.com",title: ""},
+    {name: "天猫商城",url: "https://www.tmall.com",title: ""},
+    {name: "起飞页",url: "http://www.qifeiye.com/?wd=1005",title: ""},
+    {name: "OPPO锤子官网",url: "http://www.coloros.com/topic/coloros3_1.html",title: ""},
+    {name: "阅文集团",url: "https://www.yuewen.com/#&copyright",title: ""},
+  ],
+  //博客论坛
+  [
+    {name: "博客园",url: "https://www.cnblogs.com/zhaowy/",title: ""},
+    {name: "前端之路",url: "http://www.zhaowy.cn",title: ""},
+    {name: "不落阁",url: "http://www.leo96.com/",title: ""},
+    {name: "Zblog社区",url: "http://bbs.zblogcn.com/",title: ""},
+    {name: "优秀个人博客1",url: "http://www.dpfblog.com/",title: ""},
+    {name: "刘洋博客",url: "https://www.lyblog.net/",title: ""},
+    {name: "博客112导航",url: "https://boke112.com/",title: ""},
+    {name: "小火柴的前端小册子",url: "https://xiaohuochai.site/",title: ""},
+  ]
+]
