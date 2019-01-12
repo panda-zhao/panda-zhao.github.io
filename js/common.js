0e89fc773fbb05be783089bc0c158c8c0afe5a1a
zhaowy <zhaowy@3win.com>
2019-01-12 10:22:22 +0800

    20190112(星期六) 增加管理工具

--- js/common.js
+++ js/common.js
@@ -34,6 +34,15 @@ var data = [
     {name: "Web 前端技术图谱",url: "http://www.runoob.com/w3cnote/webfrontendstack.html",title: ""},
     {name: "前端开发面试题",url: "http://www.runoob.com/w3cnote/front-end-developer-questions-and-answers.html",title: ""},
   ],
+  // 管理工具
+  [
+    {name: "GitHub",url: "https://github.com/panda-zhao",title: ""},
+    {name: "Git",url: "https://git-scm.com/",title: "目前世界上最先进的分布式版本控制系统（没有之一）。"},
+    {name: "TortoiseSVN",url: "https://tortoisesvn.net/downloads.html",title: "一个 Windows 下的版本控制系统 Apache™ Subversion® 的客户端工具。"},
+    {name: "Bugclose",url: "https://www.bugclose.com/index.html",title: "项目管理工具"},
+    {name: "图片压缩",url: "https://tinypng.com/",title: "tinypng图片压缩"},
+    {name: "坚果云",url: "https://www.jianguoyun.com/",title: ""},
+  ],
   // 前端工具
   [
     {name: "GitHub",url: "https://github.com/panda-zhao",title: ""},
@@ -49,24 +58,16 @@ var data = [
     {name: "CSS3代码生产器",url: "http://www.css3maker.com/index.html#",title: ""},
     {name: "CAN I USE?",url: "https://caniuse.com/",title: ""},
     {name: "jshint语法检查",url: "http://jshint.com/",title: ""},
-
   ],
   // 小程序开发
   [
+    {name: "微信小程序设计规范",url: "https://developers.weixin.qq.com/miniprogram/design/index.html",title: "微信小程序设计规范"},
     {name: "微信小程序API",url: "https://developers.weixin.qq.com/miniprogram/dev/api/",title: "微信小程序API"},
     {name: "微信公众平台",url: "https://mp.weixin.qq.com/",title: "服务号，订阅号，小程序等账号管理"},
     {name: "微信商户平台",url: "https://pay.weixin.qq.com/index.php/core/home/login?return_url=%2F",title: "微信商户平台"},
     {name: "百度小程序",url: "https://smartprogram.baidu.com/mappconsole/main/login",title: "百度小程序"},
   ],
-  // 个人建站
-  [
-    {name: "阿里云",url: "https://home.console.aliyun.com/new#/",title: ""},
-    {name: "百度统计",url: "https://tongji.baidu.com/web/25037646/overview/index",title: ""},
-    {name: "代码高亮",url: "http://www.592lvyou.com/?page_id=13036",title: ""},
-    {name: "制作网站logo",url: "http://www.logomaker.com.cn/",title: ""},
-    {name: "制作网站ico图标",url: "http://www.bitbug.net/",title: ""},
-    {name: "网站素材易企秀",url: "http://www.eqxiu.com/",title: "数据接口"},
-  ],
+  
   // vue.js项目
   [
     {name: "webpack中文教程",url: "https://doc.webpack-china.org/",title: ""},
@@ -84,6 +85,15 @@ var data = [
     {name: "Node.js 中文网",url: "http://nodejs.cn/api/",title: ""},
     {name: "node.js教程",url: "http://www.runoob.com/nodejs/nodejs-tutorial.html",title: ""},
     {name: "淘宝npm镜像",url: "http://npm.taobao.org/",title: ""},
+  ],
+  // 个人建站
+  [
+    {name: "阿里云",url: "https://home.console.aliyun.com/new#/",title: ""},
+    {name: "百度统计",url: "https://tongji.baidu.com/web/25037646/overview/index",title: ""},
+    {name: "代码高亮",url: "http://www.592lvyou.com/?page_id=13036",title: ""},
+    {name: "制作网站logo",url: "http://www.logomaker.com.cn/",title: ""},
+    {name: "制作网站ico图标",url: "http://www.bitbug.net/",title: ""},
+    {name: "网站素材易企秀",url: "http://www.eqxiu.com/",title: "数据接口"},
   ],
   // 案例网站
   [
