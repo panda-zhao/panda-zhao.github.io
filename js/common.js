65b44b7637478a03af8fd7442df5968311bd3b94
zhaowy <zhaowy@3win.com>
2019-01-26 18:59:31 +0800

    2019/01/26(星期六) 更新网站

--- js/common.js
+++ js/common.js
@@ -1,7 +1,32 @@
 
 
 var data = [
-  // 前端框架
+   // 1 个人收藏
+  [
+    {name: "GitHub",url: "https://github.com/panda-zhao",title: ""},
+    {name: "前端之路",url: "http://www.zhaowy.cn",title: ""},
+    {name: "博客园",url: "https://www.cnblogs.com/zhaowy/",title: ""},
+    {name: "Patreon",url: "https://www.patreon.com/",title: ""},
+  ],
+   // 2 前端学习
+   [
+    {name: "W3Cschool",url: "https://www.w3cschool.cn/",title: ""},
+    {name: "菜鸟教程",url: "http://www.runoob.com/",title: ""},
+    {name: "W3school",url: "http://www.w3school.com.cn/index.html",title: ""},
+    {name: "CSS3参考手册",url: "http://www.css88.com/book/css/quicksearch.htm",title: ""},
+    {name: "Normalize跨浏览器一致性",url: "http://necolas.github.io/normalize.css/",title: ""},
+    {name: "高德开发平台",url: "http://lbs.amap.com/api/javascript-api/guide/abc/quickstart",title: ""},
+    {name: "Web 前端技术图谱",url: "http://www.runoob.com/w3cnote/webfrontendstack.html",title: ""},
+    {name: "前端开发面试题",url: "http://www.runoob.com/w3cnote/front-end-developer-questions-and-answers.html",title: ""},
+  ],
+  // 3 JavaScript进阶
+  [
+    {name: "javascript入门",url: "https://wangdoc.com/javascript/basic/introduction.html",title: ""},
+    {name: "JavaScript 标准参考教程（alpha）",url: "http://javascript.ruanyifeng.com/",title: ""},
+    {name: "ECMAScript 6",url: "http://es6.ruanyifeng.com/#docs/intro",title: ""},
+    {name: "MDN web docs",url: "https://developer.mozilla.org/zh-CN/",title: ""},
+  ],
+  // 4 前端框架
   [
     {name: "BootCDN",url: "http://www.bootcdn.cn",title: "免费的CDN加速服务"},
     {name: "jquery",url: "http://jquery.cuishifeng.cn/",title: "jQuery API 3.3.1 速查表"},
@@ -16,25 +41,7 @@ var data = [
     {name: "jQuery UI",url: "http://jqueryui.com/themeroller/",title: ""},
     {name: "百度分享插件",url: "http://share.baidu.com/code",title: ""},
   ],
-  //JavaScript进阶
-  [
-    {name: "javascript入门",url: "https://wangdoc.com/javascript/basic/introduction.html",title: ""},
-    {name: "JavaScript 标准参考教程（alpha）",url: "http://javascript.ruanyifeng.com/",title: ""},
-    {name: "ECMAScript 6",url: "http://es6.ruanyifeng.com/#docs/intro",title: ""},
-    {name: "MDN web docs",url: "https://developer.mozilla.org/zh-CN/",title: ""},
-  ],
-  //前端学习
-  [
-    {name: "W3Cschool",url: "https://www.w3cschool.cn/",title: ""},
-    {name: "菜鸟教程",url: "http://www.runoob.com/",title: ""},
-    {name: "W3school",url: "http://www.w3school.com.cn/index.html",title: ""},
-    {name: "CSS3参考手册",url: "http://www.css88.com/book/css/quicksearch.htm",title: ""},
-    {name: "Normalize跨浏览器一致性",url: "http://necolas.github.io/normalize.css/",title: ""},
-    {name: "高德开发平台",url: "http://lbs.amap.com/api/javascript-api/guide/abc/quickstart",title: ""},
-    {name: "Web 前端技术图谱",url: "http://www.runoob.com/w3cnote/webfrontendstack.html",title: ""},
-    {name: "前端开发面试题",url: "http://www.runoob.com/w3cnote/front-end-developer-questions-and-answers.html",title: ""},
-  ],
-  // 项目管理工具
+  // 5 项目管理工具
   [
     {name: "GitHub",url: "https://github.com/panda-zhao",title: ""},
     {name: "Git",url: "https://git-scm.com/",title: "目前世界上最先进的分布式版本控制系统（没有之一）。"},
@@ -43,7 +50,7 @@ var data = [
     {name: "图片压缩",url: "https://tinypng.com/",title: "tinypng图片压缩"},
     {name: "坚果云",url: "https://www.jianguoyun.com/",title: ""},
   ],
-  // 前端工具
+  // 6 前端工具
   [
     {name: "脚本之家在线工具",url: "http://tools.jb51.net/",title: ""},
     {name: "雪碧图合成",url: "https://www.toptal.com/developers/css/sprite-generator",title: ""},
@@ -56,10 +63,8 @@ var data = [
     {name: "CSS3代码生产器",url: "http://www.css3maker.com/index.html#",title: ""},
     {name: "CAN I USE?",url: "https://caniuse.com/",title: ""},
     {name: "jshint语法检查",url: "http://jshint.com/",title: ""},
-    
-    
   ],
-  // 小程序开发
+  // 7 小程序开发
   [
     {name: "微信小程序设计规范",url: "https://developers.weixin.qq.com/miniprogram/design/index.html",title: "微信小程序设计规范"},
     {name: "微信小程序API",url: "https://developers.weixin.qq.com/miniprogram/dev/api/",title: "微信小程序API"},
@@ -68,7 +73,7 @@ var data = [
     {name: "百度小程序",url: "https://smartprogram.baidu.com/mappconsole/main/login",title: "百度小程序"},
   ],
   
-  // vue.js项目
+  // 8 vue.js项目
   [
     {name: "webpack中文教程",url: "https://doc.webpack-china.org/",title: ""},
     {name: "Vue.js教程",url: "https://cn.vuejs.org/v2/guide/installation.html",title: ""},
@@ -79,16 +84,21 @@ var data = [
     {name: "Store.js",url: "https://www.bootcdn.cn/store.js/readme/",title: ""},
     {name: "ElementUI教程",url: "http://element-cn.eleme.io/#/zh-CN/component/installation",title: ""},
   ],
-  // 服务器端
+  // 9 服务器端
   [
     {name: "Express",url: "http://www.expressjs.com.cn/",title: ""},
     {name: "Node.js 中文网",url: "http://nodejs.cn/api/",title: ""},
     {name: "node.js教程",url: "http://www.runoob.com/nodejs/nodejs-tutorial.html",title: ""},
     {name: "淘宝npm镜像",url: "http://npm.taobao.org/",title: ""},
-    {name: "nodemailer",url: "https://nodemailer.com/about/",title: "nodeJs发送邮件模块"},
-    {name: "redis",url: "https://redis.io/",title: "key-value存储系统"},
+    
   ],
-  // 个人建站
+  //10 数据库
+  [
+    {name: "nodemailer",url: "https://nodemailer.com/about/",title: "nodeJs发送邮件模块"},
+    {name: "postgresDB",url: "https://node-postgres.com/",title: "关系型数据库" },
+    {name: "redis",url: "http://www.redis.cn/download.html",title: "key-value存储系统"},
+  ],
+  // 11 个人建站
   [
     {name: "阿里云",url: "https://home.console.aliyun.com/new#/",title: ""},
     {name: "百度统计",url: "https://tongji.baidu.com/web/25037646/overview/index",title: ""},
@@ -97,7 +107,7 @@ var data = [
     {name: "制作网站ico图标",url: "http://www.bitbug.net/",title: ""},
     {name: "网站素材易企秀",url: "http://www.eqxiu.com/",title: "数据接口"},
   ],
-  // 案例网站
+  // 12 案例网站
   [
     {name: "网易阴阳师官网",url: "https://yys.163.com",title: ""},
     {name: "天猫商城",url: "https://www.tmall.com",title: ""},
@@ -105,10 +115,9 @@ var data = [
     {name: "OPPO锤子官网",url: "http://www.coloros.com/topic/coloros3_1.html",title: ""},
     {name: "阅文集团",url: "https://www.yuewen.com/#&copyright",title: ""},
   ],
-  //博客论坛
+  //13 博客论坛
   [
-    {name: "博客园",url: "https://www.cnblogs.com/zhaowy/",title: ""},
-    {name: "前端之路",url: "http://www.zhaowy.cn",title: ""},
+    
     {name: "不落阁",url: "http://www.leo96.com/",title: ""},
     {name: "Zblog社区",url: "http://bbs.zblogcn.com/",title: ""},
     {name: "优秀个人博客1",url: "http://www.dpfblog.com/",title: ""},
