1dfc94d287236aba43e1f9fd2f2a5707da55b403
zhaowy <zhaowy@3win.com>
2019-02-28 09:36:31 +0800

    2019/02/28(星期四) 添加电影宝

--- js/common.js
+++ js/common.js
@@ -10,6 +10,7 @@ var data = [
     {name: "阿里云",url: "https://home.console.aliyun.com/new#/",title: ""},
     {name: "Patreon",url: "https://www.patreon.com/",title: ""},
     {name: "星座网",url: "http://www.xzw.com/",title: ""},
+    {name: "电影宝",url: "http://www.dyb365.com",title: ""}
     
   ],
  
@@ -102,6 +103,8 @@ var data = [
     {name: "node.js教程",url: "http://www.runoob.com/nodejs/nodejs-tutorial.html",title: ""},
     {name: "淘宝npm镜像",url: "http://npm.taobao.org/",title: ""},
     {name: "nodemailer",url: "https://nodemailer.com/about/",title: "nodeJs发送邮件模块"},
+    {name: "multer",url: "https://github.com/expressjs/multer",title: "文件上传模块"},
+    
   ],
   //10 数据库
   [
