c33df603f91a5b91f27a1b2bd9aa23f06ea61003
zhaowy <82626363@qq.com>
2019-08-12 11:32:58 +0800

    updata 更新省政务帮助

--- js/baseData.js
+++ js/baseData.js
@@ -56,3 +56,95 @@ var nationArray = {
   "55": "珞巴",
   "56": "基诺"
 }
+// 白云小程序入口配置
+const entryConfig = {
+  "wholeSetText": "智能办理企业名称自主申报 + 设立登记 + 印章申请 + 银行开户 + 税务登记发票申领",
+  "menus": [
+    {
+      "page": "/pages/webView/chooseAddress/main",
+      "icon": "https://6563-ec-gov-biz-mp-19a952-1258264808.tcb.qcloud.la/ec/icon/choose-address.png?sign=e3f7ea755cd90b86b4940ec940af8ca7&t=1560766978",
+      "name": "办公选址",
+      "width": 57,
+      "height": 69
+    },
+    {
+      "page": "/pages/webView/name/main",
+      "icon": "https://6563-ec-gov-biz-mp-19a952-1258264808.tcb.qcloud.la/ec/icon/name.png?sign=c0918a81038a021f6f2acb793c15191a&t=1560766955",
+      "name": "智能精算<br>公司起名",
+      "width": 65,
+      "height": 63
+    },
+    {
+      "code": "WHOLE_SET_APPLY",
+      "icon": "https://6563-ec-gov-biz-mp-19a952-1258264808.tcb.qcloud.la/ec/icon/check-name.png?sign=d5121f32847436bd4a4436a49febe833&t=1560766256",
+      "name": "名称申报<br>设立登记",
+      "belong": [
+        "FOSHAN"
+      ],
+      "width": 74,
+      "height": 65
+    },
+    {
+      "code": "WEIXIN_APPLY",
+      "icon": "https://6563-ec-gov-biz-mp-19a952-1258264808.tcb.qcloud.la/ec/icon/check-name.png?sign=d5121f32847436bd4a4436a49febe833&t=1560766256",
+      "name": "名称申报<br>设立登记",
+      "belong": [
+        "JIANGMEN"
+      ],
+      "width": 74,
+      "height": 65
+    },
+    {
+      "code": "NAME_APPLY",
+      "icon": "https://6563-ec-gov-biz-mp-19a952-1258264808.tcb.qcloud.la/ec/icon/check-name.png?sign=d5121f32847436bd4a4436a49febe833&t=1560766256",
+      "name": "名称自主<br>申报",
+      "width": 74,
+      "height": 65
+    },
+    {
+      "code": "REG_APPLY",
+      "icon": "https://6563-ec-gov-biz-mp-19a952-1258264808.tcb.qcloud.la/ec/icon/sign-up.png?sign=48f6ba15446a97cf244b05b60dbb4dc5&t=1560766281",
+      "name": "设立登记",
+      "width": 68,
+      "height": 68
+    },
+    {
+      "code": "STAMP_RECORD",
+      "icon": "https://6563-ec-gov-biz-mp-19a952-1258264808.tcb.qcloud.la/ec/icon/stamp.png?sign=2a7b1ddfe935b66d9380e34dbedd4c91&t=1560766306",
+      "name": "印章申请",
+      "width": 59,
+      "height": 75
+    },
+    {
+      "code": "BANK_ACCOUNT",
+      "icon": "https://6563-ec-gov-biz-mp-19a952-1258264808.tcb.qcloud.la/ec/icon/bank.png?sign=d45e8eb2fe71eaa70007432d7c8107b3&t=1560766320",
+      "name": "银行开户",
+      "width": 63,
+      "height": 65
+    },
+    {
+      "code": "SHUIWU_APPLY",
+      "icon": "https://6563-ec-gov-biz-mp-19a952-1258264808.tcb.qcloud.la/ec/icon/tax.png?sign=9e796137d8222818721a5ca7ea383cea&t=1560766336",
+      "name": "税务登记<br>发票申领",
+      "width": 76,
+      "height": 61
+    },
+    {
+      "code": "SOCIAL_SECURITY",
+      "icon": "https://6563-ec-gov-biz-mp-19a952-1258264808.tcb.qcloud.la/ec/icon/protection.png?sign=21b4d9f749144e636193f7d7386b669f&t=1560766348",
+      "name": "社保开户",
+      "belong": [
+        "GUANGZHOU"
+      ],
+      "width": 74,
+      "height": 59
+    },
+    {
+      "icon": "https://6563-ec-gov-biz-mp-19a952-1258264808.tcb.qcloud.la/ec/icon/fund.png?sign=1bcff16e6159874864a82c3f26b1fa24&t=1560766369",
+      "name": "公积金开户",
+      "width": 61,
+      "height": 62
+    }
+  ]
+}
+
