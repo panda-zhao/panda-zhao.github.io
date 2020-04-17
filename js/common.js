a483d71430d54a3fe9f7253eb9e5dedab52d88a3
zhaowy <82626363@qq.com>
2020-04-17 17:49:34 +0800

    【APPEND】新增电子税务局

--- js/common.js
+++ js/common.js
@@ -1,8 +1,6 @@
 var data = [
   //一、常用网站
   [
-    {name: "红盾网",url: "http://scjgj.gz.gov.cn/gzaic/WebItem/Index.html",title: "网上办事大厅"},
-    {name: "广东政务网",url: "http://59.41.9.112:90/ewb/login.html",title: "广州市开办企业一网通"},
     {name: "GitHub",url: "https://github.com/panda-zhao",title: ""},
     {name: "技术支持",url: "./support/index.html",title: "技术支持"},
     {name: "常用速查表",url: "https://www.w3cschool.cn/dict/",title: "各大语言速查表"},
@@ -178,6 +176,9 @@ var data = [
     // {name: "博客园",url: "https://www.cnblogs.com/zhaowy/",title: ""},
   ],
   [
+    {name: "红盾网",url: "http://scjgj.gz.gov.cn/gzaic/WebItem/Index.html",title: "网上办事大厅"},
+    {name: "广东政务网",url: "http://59.41.9.112:90/ewb/login.html",title: "广州市开办企业一网通"},
+    {name: "广东电子税务局",url: "https://www.etax-gd.gov.cn/xxmh/html/index.html",title: "广东电子税务局"},
     {name: "国家社会保险公共服务平台",url: "http://si.12333.gov.cn/index.jhtml",title: "国家社会保险公共服务平台"},
     {name: "国家企业信用信息公示系统",url: "http://www.gsxt.gov.cn/corp-query-homepage.html",title: "国家社会保险公共服务平台"},
     {name: "河南省社会保障卡服务网",url: "http://www.ha.12333.gov.cn/",title: "河南省社会保障卡服务网"}
