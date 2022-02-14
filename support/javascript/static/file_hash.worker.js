7f716456ded40ed08830186ae541b69500e5ab4f
zhaowy <82626363@qq.com>
2022-02-14 12:00:40 +0800

    新增rsa加解密

---
+++ support/javascript/static/file_hash.worker.js
@@ -0,0 +1,29 @@
+/*
+ * @Author: zhaowy
+ * @Date: 2022-02-14 11:13:16
+ * @LastEditors: zhaowy
+ * @LastEditTime: 2022-02-14 11:13:54
+ * @FilePath: \panda-zhao.github.io\support\javascript\static\hash.js
+ * @Description: 
+ */
+importScripts('https://cdn.bootcss.com/crypto-js/3.1.9-1/crypto-js.min.js');
+    let algo;
+    function createAlgo(name){
+      switch(name){
+        case 'MD5':algo=CryptoJS.algo.MD5.create();break;
+        case 'SHA-1':algo=CryptoJS.algo.SHA1.create();break;
+        case 'SHA-224':algo=CryptoJS.algo.SHA224.create();break;
+        case 'SHA-256':algo=CryptoJS.algo.SHA256.create();break;
+        case 'SHA-384':algo=CryptoJS.algo.SHA384.create();break;
+        case 'SHA-512':algo=CryptoJS.algo.SHA512.create();break;
+      }
+    }
+    addEventListener('message', function(e){
+      switch(e.data.type){
+        case 'algo':createAlgo(e.data.name);break;
+        case 'chunk':algo.update(CryptoJS.enc.Latin1.parse(e.data.chunk));
+        postMessage({type:'progress',value:e.data.offs*100/e.data.total});break;
+        case 'done':let hash=algo.finalize();
+        postMessage({type:'result',value:hash.toString(CryptoJS.enc.Hex)});self.close();break;
+      }
+    },false);
