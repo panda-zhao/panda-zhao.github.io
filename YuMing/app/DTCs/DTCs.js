81b0dfde06a41368d11a0fc6e61213a115600979
赵伟扬 <82626363@qq.com>
2024-04-16 09:21:38 +0800

    挑战YMOBD

---
+++ YuMing/app/DTCs/DTCs.js
@@ -0,0 +1,61 @@
+
+// import bn from './bn.json' // 孟加拉语
+// import de from './de.json' // 德语
+// import en from './en.json' // 英语
+// import es from './es.json' // 西班牙语
+// import fi from './fi.json' // 芬兰语
+// import fr from './fr.json' // 法语
+// import hi from './hi.json' // 印地语
+// import hu from './hu.json' // 匈牙利语
+// import it from './it.json' // 意大利语
+// import ja from './ja.json' // 日语
+// import kr from './kr.json' // 韩语
+// import nl from './nl.json' // 荷兰语
+// import pl from './pl.json' // 波语
+// import pt from './pt.json' // 葡萄牙语
+// import ru from './ru.json' // 俄语 
+// import sv from './sv.json' // 瑞典语
+
+// import Hans from './zh-Hans.json' // 简体中文
+// import Hant from './zh-Hant.json' // 繁体中文
+
+// import cs from './cs.json' // 捷克语
+// import da from './da.json' // 丹麦语
+// import ms from './ms.json' // 马来语
+// import ro from './ro.json' // 罗马尼亚语
+// import th from './th.json' // 泰语
+// import tr from './tr.json' // 土耳其语
+// export default {
+// 	cs,
+// 	da,
+// 	ms,
+// 	ro,
+// 	th,
+// 	tr,
+	
+// 	bn,
+// 	de,
+// 	en,
+// 	es,
+// 	fi,
+// 	fr,
+// 	hi,
+// 	hu,
+// 	it,
+// 	ja,
+// 	kr,
+// 	nl,
+// 	pl,
+// 	pt,
+// 	ru,
+// 	sv,
+// 	'zh-Hans': Hans,
+// 	'zh-Hant': Hant
+// }
+
+import en from './en.json' // 英语
+import Hans from './zh-Hans.json' // 简体中文
+export default {
+	en,
+	'zh-Hans': Hans,
+}
